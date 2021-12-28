55be2e1aaf373a6a9d665a6e8cc677db88624a3e
mor-sa <mor.saranga@gmail.com>
2021-12-28 15:22:37 +0200

    continue server

--- Server.py
+++ Server.py
@@ -1,10 +1,22 @@
 import socket
 import scapy.all
+from termcolor import colored, COLORS
 
-UDP_PORT = 13117
+UDP_DEST_PORT = 13117
+CLASS_PORT = 2063
 MAGIC_COOKIE = 0xabcddcba
-UDP_PORT = 2063
+MESSAGE_TYPE = 0x2
+IP_ADDRESS = "127.0.0.1"
 
 class Server:
     def __init__(self):
+        self.alive_status = False
+
+    def start():
+
+        self.alive_status = True
         
+
+# if __name__ == '__main__':
+#     print(COLORS)
+#     print(colored("hello red","green","on_grey"))
